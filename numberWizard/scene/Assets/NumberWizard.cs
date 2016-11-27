30d1a1e87a3a47d37ca49bb50f2d4075703c3314
Machadch <chris.machado@live.com>
2016-11-26 22:40:28 -0800

    UI added for number game

---
+++ numberWizard/scene/Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	
+	public int MaxGuessesAllowed = 7;
+	public Text text;
+	
+	void Start () {
+		StartGame(); 
+	}
+	
+	void StartGame() {
+		max = 1000;
+		min = 1;
+		guess = 500;
+		max += 1;
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess ();
+	}
+	
+	public void GuessLower() {
+		max = guess;
+		NextGuess ();
+	}
+	
+	
+	void NextGuess () {
+		guess = (max + min) / 2;
+		text.text = guess.ToString();
+		MaxGuessesAllowed -= 1;
+		if (MaxGuessesAllowed <= 0){
+			Application.LoadLevel("Win");
+		}
+	}
+}

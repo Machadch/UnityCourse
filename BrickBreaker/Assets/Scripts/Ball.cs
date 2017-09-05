4af6c4ef10f2d8ea6721f9a250f19cd49e8de346
Machadch <chris.machado@live.com>
2017-09-04 21:15:00 -0700

    BrickBreak coming along

---
+++ BrickBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	private Paddle paddle;
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+	
+	// Use this for initialization
+	void Start () {
+	paddle = GameObject.FindObjectOfType<Paddle>();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		print(paddleToBallVector.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+		//Lock the ball to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			if (Input.GetMouseButtonDown(0)) {
+			//Launch the Ball
+				print("Clicked");
+				this.rigidbody2D.velocity = new Vector2 (4f, 10f);
+				hasStarted = true;
+			}
+		}
+	}
+}
---
+++ BrickBreaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	private int timesHit;
+	private LevelManager levelManager;
+	private bool isBreakable;
+	public Sprite[] hitSprites;
+	public static int BricksInLevel;
+	
+	// Use this for initialization
+	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			BricksInLevel++;
+			print (BricksInLevel);
+		}
+		
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		if (isBreakable) {
+			HandleHit();
+		}
+	}
+	
+	void HandleHit() {
+		int maxHits = hitSprites.Length + 1;
+		this.timesHit += 1;
+		if (timesHit >= maxHits) {
+			Destroy(gameObject);
+			BricksInLevel -= 1;
+			levelManager.BrickDestroyed();
+		} else {
+			LoadSprites();
+		}
+	}
+	
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites[spriteIndex]) {
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
+	}
+	
+	void SimulateWin () {
+		levelManager.LoadNextLevel();
+	}
+}
